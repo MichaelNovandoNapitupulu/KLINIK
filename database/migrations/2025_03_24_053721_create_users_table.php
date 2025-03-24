fc0867539c480d486178cd4e7693d74092c0c489
Michael Novando Napitupulu <email@example.com>
2025-03-24 16:46:03 +0700

    Tugas kedua

---
+++ database/migrations/2025_03_24_053721_create_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration {
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 255);
+            $table->string('alamat', 255)->nullable();
+            $table->string('no_hp', 50)->unique();
+            $table->string('email', 50)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->enum('role', ['pasien', 'dokter'])->default('pasien');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
